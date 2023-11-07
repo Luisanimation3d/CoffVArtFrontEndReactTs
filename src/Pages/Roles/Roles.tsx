b072b5ca33062674e4ed2202e05b6a706739bb60
Luisanimation3d <luisanimation3d@gmail.com>
2023-11-06 21:11:58 -0500

    Se mejoró la UX del componente Table, se corrigieron algunos bugs del Modal y se añadió detalles en Roles.tsx

--- src/Pages/Roles/Roles.tsx
+++ src/Pages/Roles/Roles.tsx
@@ -1,12 +1,16 @@
-import {Column} from "../../types/Table";
-import {Table} from "../../components/Table/Table.tsx";
-import {Titles} from "../../components/Titles/Titles.tsx";
-import {Container} from "../../components/Container/Container.tsx";
 import {useState} from "react";
-import {SearchInput} from "../../components/SearchInput/SearchInput.tsx";
+import {Column} from "../../types/Table";
+import {Table} from "../../components/Table/Table";
+import {Titles} from "../../components/Titles/Titles";
+import {Container} from "../../components/Container/Container";
+import {SearchInput} from "../../components/SearchInput/SearchInput";
+import {Modal, ModalContainer} from "../../components/Modal/Modal.tsx";
+import {createPortal} from "react-dom";
 
 export const Roles = () => {
     const [search, setSearch] = useState<string>('')
+    const [isModalOpen, setIsModalOpen] = useState<boolean>(false)
+    const [dataModal, setDataModal] = useState<any>({})
     const columnsRoles: Column[] = [
         {
             key: 'id',
@@ -30,17 +34,43 @@ export const Roles = () => {
         },
         {
             id: 2,
-            name: 'User',
+            name: 'Usuario',
             description: 'this is the user role',
         },
     ]
 
     let dataRolesFiltered: any;
 
-    if(search.length > 0){
+    if (search.length > 0) {
         dataRolesFiltered = dataRoles.filter(role => role.name.toLowerCase().includes(search.toLowerCase()) || role.description.toLowerCase().includes(search.toLowerCase()))
-    }else{
+    } else {
         dataRolesFiltered = dataRoles
+    }
+
+    const handleRowClick = (row: any) => {
+        const detailRole = {
+            name: row.name,
+            details: [
+                {
+                    id: 1,
+                    permission: 'usuarios',
+                    privilege: 'lectura',
+                },
+                {
+                    id: 2,
+                    permission: 'usuarios',
+                    privilege: 'escritura',
+                },
+                {
+                    id: 3,
+                    permission: 'roles',
+                    privilege: 'lectura',
+                }
+            ]
+        }
+
+        setDataModal(detailRole)
+        setIsModalOpen(true)
     }
 
     return (
@@ -48,16 +78,46 @@ export const Roles = () => {
             <Container>
                 <Titles title={'Roles'} level={1}/>
                 <div className="roles__table">
-                    <SearchInput label={'Buscar Roles'} onChange={e=> setSearch(e.target.value)} value={search} idSearch={'roleSearch'} />
-                    <Table columns={columnsRoles} data={dataRolesFiltered} onRowClick={()=> null} editableAction={{
+                    <SearchInput label={'Buscar Roles'} onChange={e => setSearch(e.target.value)} value={search}
+                                 idSearch={'roleSearch'}/>
+                    <Table columns={columnsRoles} data={dataRolesFiltered} onRowClick={handleRowClick} editableAction={{
                         onClick: () => null,
                     }}
-                    deleteAction={{
-                        onClick: () => null,
-                    }}
+                           deleteAction={{
+                               onClick: () => null,
+                           }}
                     />
                 </div>
             </Container>
+            {
+                isModalOpen && createPortal(
+                    <>
+                        <ModalContainer ShowModal={setIsModalOpen}>
+                            <Modal showModal={setIsModalOpen} title={`Rol ${dataModal.name}`}>
+                                <div className="modal__content" style={{
+                                    padding: '1rem',
+                                }}>
+                                    <Table columns={[
+                                        {
+                                            key: 'id',
+                                            header: 'ID',
+                                        },
+                                        {
+                                            key: 'permission',
+                                            header: 'Permiso',
+                                        },
+                                        {
+                                            key: 'privilege',
+                                            header: 'Privilegio',
+                                        }
+                                    ]} data={dataModal.details} onRowClick={() => null}/>
+                                </div>
+                            </Modal>
+                        </ModalContainer>
+                    </>,
+                    document.getElementById('modal') as HTMLElement
+                )
+            }
         </>
     )
 }
