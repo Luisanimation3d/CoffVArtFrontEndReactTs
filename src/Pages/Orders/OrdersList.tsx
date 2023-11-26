9226859b57d9b2aae1842f11b5213110ed9a50c9
MateoOsorio016 <mosorio7347@misena.edu.co>
2023-11-25 23:48:45 -0500

    Orders get

--- src/Pages/Orders/OrdersList.tsx
+++ src/Pages/Orders/OrdersList.tsx
@@ -26,7 +26,7 @@ export const Orders = () => {
         { key: 'id', header: 'ID' },
         { key: 'code', header: 'Codigo' },
         { key: 'total', header: 'Total' },
-        { key: 'coustumerId', header: 'Cliente' },
+        { key: 'customerId', header: 'Cliente' },
         { key: 'state', header: 'Estado' },
     ];
     
@@ -67,6 +67,24 @@ export const Orders = () => {
         }, 500);
     }; 
 
+    const [orderDetails, setOrderDetails] = useState<any[]>([]);
+
+    const getOrderDetails =  (order: any) => {
+        console.log(order, "Esta es la orden")
+        const orderDetails= order?.ordersderails?.map((orderDetail: any) => ({
+            id: orderDetail.id,
+            orderId: orderDetail.orderId,
+            product: orderDetail.product.name,
+            quantity: orderDetail.quantity,
+            value: orderDetail.value,
+            total: orderDetail.quantity*orderDetail.value,
+        }));
+        setOrderDetails(orderDetails);
+        setIsModalOpen(true);
+        }
+    
+    
+console.log(data)
     return (
         <>
             <Container align={'CENTER'} justify={'TOP'}>
@@ -104,7 +122,7 @@ export const Orders = () => {
                             <Table
                                 columns={columnsOrders}
                                 data={dataOrdersFiltered}
-                                onRowClick={() => setIsModalOpen(true)}
+                                onRowClick={getOrderDetails}
                                 editableAction={{
                                     onClick: (row) => navigate(`/admin/Orders/edit/${row.id}`),
                                 }}
@@ -123,39 +141,32 @@ export const Orders = () => {
                             title="Detalle de Pedido"
                             showModal={setIsModalOpen}
                         >
+                            
                             <Table
                                 columns={[
                                     {
-                                        key: "producto",
+                                        key: "product",
                                         header: "Producto",
+                                        // render: (row) => row.product ? row.product.name : 'N/A',
                                     },
                                     {
-                                        key: "cantidad",
+                                        key: "quantity",
                                         header: "Cantidad",
+                                        // render: (row) => row.quantity !== undefined ? row.quantity : 'N/A',
                                     },
                                     {
-                                        key: "valorUnitario",
+                                        key: "value",
                                         header: "Valor Unitario",
+                                        // render: (row) => row.value !== undefined ? row.value : 'N/A',
                                     },
                                     {
                                         key: "total",
                                         header: "Total",
+                                        // render: (row) => (row.quantity !== undefined && row.value !== undefined) ? row.quantity * row.value : 'N/A',
                                     },
+                                    // You can add more columns as needed
                                 ]}
-                                data={[
-                                    {
-                                        producto: "Producto 1",
-                                        cantidad: 5,
-                                        valorUnitario: 10,
-                                        total: 50,
-                                    },
-                                    {
-                                        producto: "Producto 2",
-                                        cantidad: 3,
-                                        valorUnitario: 15,
-                                        total: 45,
-                                    },
-                                ]}
+                                data={orderDetails}
                                 onRowClick={() => null}
                             />
                         </Modal>
