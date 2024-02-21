bb9b57dda31316710f89be3a31c58c50ae02deb0
AlejandroVelasHel <velalejandro23@gmail.com>
2024-02-21 14:47:28 -0500

    implementación de la creacion de productos a medias

--- src/Modales/EditProcessModal/EditProcessOModal.tsx
+++ src/Modales/EditProcessModal/EditProcessOModal.tsx
@@ -8,15 +8,36 @@ import {Button} from "../../components/Button/Button.tsx";
 import { useNavigate} from "react-router-dom";
 
 export const EditProcessOModal = ({id,setIsModalOpen, title = 'Cambiar proceso'}: { id: number , setIsModalOpen: (value: boolean) => void, title?: string }) => {
+    const [formValues, setFormValues] = useState<{newQuantity:number}>({
+        newQuantity: 0,
+    })
     const [process, setProcess] = useState<SelectOption | undefined>();
+    const [products, setProducts] = useState<SelectOption | undefined>();
     const navigate = useNavigate()
+    const [openView,setOpenViewP]= useState<boolean>(false);
     const {data, put, get, loading, error: errorRegister} = useFetch(API_URL)
     const { data: processData, get: getProcess } = useFetch(API_URL);
+    const { data: productsData, get: getProducts } = useFetch(API_URL);
     const [options, setOptions] = useState<SelectOption[]>([]);
+    const [optionsP, setOptionsP] = useState<SelectOption[]>([]);
+
     useEffect(() => {
         get(`productionOrders/${id}?apikey=${API_KEY}`)
     }, []);
-
+    useEffect(() => {
+        setOpenViewP(false)
+        getProducts(`products?apikey=${API_KEY}`);
+    }, []);
+    useEffect(() => {
+        if (productsData?.products?.rows) {
+          const products = productsData?.products?.rows?.map((products: any) => ({
+            label: products.name,
+            value: products.id,
+          }));
+          setOptionsP(products);
+        }
+      }, [productsData]);
+      console.log(productsData,"datos de productos")
     useEffect(() => {
         getProcess(`processes?apikey=${API_KEY}`);
     }, []);
@@ -30,6 +51,7 @@ export const EditProcessOModal = ({id,setIsModalOpen, title = 'Cambiar proceso'}
           setOptions(process);
         }
       }, [processData]);
+
     useEffect(() => {
         if (!loading) {
             const newValues = {
@@ -38,12 +60,26 @@ export const EditProcessOModal = ({id,setIsModalOpen, title = 'Cambiar proceso'}
             setRegisterForm(newValues)
         }
     }, [data]);
+    useEffect(() => {
+        if (!loading) {
+            const newValuesP = {
+                productId: data?.productionOrder?.productId
+            }
+            setRegisterFormP(newValuesP)
+        }
+    }, [data]);
     console.log(data,"data")
     const [error, setError] = useState<{ [key: string]: string }>({})
     const [registerForm, setRegisterForm] = useState<{
         processId: string,
     }>({
         processId: '',
+
+    })
+    const [registerFormP, setRegisterFormP] = useState<{
+        productId: string,
+    }>({
+        productId: '',
 
     })
     const formFieldsRegister: FormField[] = [ 
@@ -58,7 +94,38 @@ export const EditProcessOModal = ({id,setIsModalOpen, title = 'Cambiar proceso'}
 
         }
 
-    ]
+    ];
+    const formFieldsQuantity: FormField[] = [ 
+        
+        {
+                name: 'productId',
+                type: 'select',
+                label: 'Producto',
+                value: products,
+                options: optionsP,
+                onChange: (o) => setProducts(o),
+                size: 'large',
+    
+        }
+        ,
+        {
+            name: 'newQuantity',
+            type: 'number',
+            label: 'Paquetes de café',
+            placeholder: '0',
+            value: formValues['newQuantity']!== undefined ? String(formValues['newQuantity']): '',
+            onChange: (value) => handleInputChange('newQuantity',value),
+            size: 'large',
+
+        }
+
+    ];
+    const handleInputChange = (name: string, value: string | number) => {
+        setFormValues(prevValues => ({
+            ...prevValues,
+            [name]: value
+        }));
+    };
     console.log(registerForm.processId,'register proceso')
     const validateForm = () => {
         const errors: any = {}
@@ -69,6 +136,20 @@ export const EditProcessOModal = ({id,setIsModalOpen, title = 'Cambiar proceso'}
         
         return errors
     }
+    const validateFormRecived = () => {
+        const errors: any = {}
+         console.log(formValues,"values",products," products")
+            if (!formValues.newQuantity ) {
+                errors.newQuantity = 'La cantidad es requerida';
+            }
+            if (!products?.value || products?.value === '') {
+                errors.productId = 'El producto es requerido';
+            }
+        
+        
+        return errors
+    }
+    
     const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         const errorsForm = validateForm();
@@ -82,9 +163,27 @@ export const EditProcessOModal = ({id,setIsModalOpen, title = 'Cambiar proceso'}
         console.log(requestBody, "aquí body")
         put(`productionOrders/${id}?apikey=${API_KEY}`, requestBody)
         console.log(process,"aquí process")
-    }; useEffect(()=> {
+    };
+    const handleSubmitRecived = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        const errorsForm = validateFormRecived();
+        console.log(errorsForm,"aquí error")
+        if(Object.keys(errorsForm).length !== 0) {
+            setError(errorsForm)
+            return
+        }
+        const requestBody = {
+            productId:products?.value,
+            newQuantity: formValues.newQuantity,
+        };
+        console.log(requestBody)
+        put(`productionOrders/${id}?apikey=${API_KEY}`, requestBody)
+    };
+    useEffect(()=> {
         if(data && !loading && !errorRegister){
-            console.log("pedrito")
+            if(process?.value == 5 ){
+                navigate(`/admin/ProductionOrders/edit/${id}`)
+            }
         }
     },[data])
     if (loading) {
@@ -97,11 +196,20 @@ export const EditProcessOModal = ({id,setIsModalOpen, title = 'Cambiar proceso'}
                     width: '100%',
                     padding: '1rem 2rem',
                 }}>
-                    <Form fields={formFieldsRegister} button={<Button text={title} type={'SUBMIT'}/>}
+                    {
+                        !openView?(
+                        <Form fields={formFieldsRegister} button={<Button text={title} type={'SUBMIT'}/>}
                           onSubmit={handleSubmit}
                           cancelButton={false}
                           errors={error}
-                    />
+                    />):(
+                        <Form fields={formFieldsQuantity} button={<Button text={'Guardar Cantidad'} type={'SUBMIT'}/>}
+                          onSubmit={handleSubmitRecived}
+                          cancelButton={false}
+                          errors={error}/>
+                    )
+                    }
+                    
                 </div>
             </Modal>
         </ModalContainer>
--- src/Pages/ProductionOrders/ProductionOrdersEdit.tsx
+++ src/Pages/ProductionOrders/ProductionOrdersEdit.tsx
@@ -1,104 +1,299 @@
-import React, {useState, useEffect} from 'react';
-import {useFetch} from '../../hooks/useFetch';
-import {FormField, SelectOption} from '../../types/Form';
-import {Button} from '../../components/Button/Button';
-import {Form} from '../../components/Form/Form';
-import {API_KEY} from '../../constantes';
-import {useParams, useNavigate} from 'react-router-dom';
+
+import {useEffect, useState} from "react";
+
+import {Container} from "../../components/Container/Container.tsx";
+import {Titles} from "../../components/Titles/Titles.tsx";
+import {Column} from "../../types/Table";
+import {Table} from "../../components/Table/Table.tsx";
+import {Form} from "../../components/Form/Form.tsx";
+import {FormField, SelectOption} from "../../types/Form";
+import {Button} from "../../components/Button/Button.tsx";
+import {useFetch} from "../../hooks/useFetch.tsx";
+import {API_KEY, API_URL} from "../../constantes.ts";
+
+export const IncrementProducts = () => {
+    const {
+        data: dataProducts,
+        loading: loadingProducts,
+        error: errorProducts,
+        get: getProducts
+    } = useFetch(API_URL)
+    const {
+        data: dataPO,
+        loading: loadingPO,
+        error: errorPO,
+        get: getPO
+    } = useFetch(API_URL)
+    const [detalles, setDetalles] = useState<any[]>([]);
+    const [productionOrder, setProductionOrder] = useState<string>('');
+    const [selectProductionOrder, setSelectProductionOrder] = useState<SelectOption | undefined>(undefined);
+    const [selectProduct, setSelectProduct] = useState<SelectOption | undefined>(undefined);
+    const [cantidad, setCantidad] = useState('');
+    const [productionOrders, setProductionOrders] = useState<SelectOption[]>([]);
+    const [products, setProducts] = useState<SelectOption[]>([]);
 
 
-export const ProductionOrdersEdit = () => {
-    const {id} = useParams<{ id: string }>()
-    const navigate = useNavigate()
-    const [formValues, setFormValues] = useState<Record<string, string  | number>>({
-        orderNumber: '',
-        quantity: '',
-    })
-    const [error, setError] = useState<{}>({});
-    const handleInputChangen = (value: SelectOption | undefined, name: string | number) => {
-        setFormValues((prevValues: any) => ({
-            ...prevValues,
-            [name]:value,
-        }));
-    };
-    const productionOrderFields: FormField[] = [
+    const headers: Column[] = [
         {
-            name: 'orderNumber',
-            type: 'text',
-            label: 'Numero de la orden',
-            placeholder: '1',
-            value: formValues.orderNumber !== undefined ? String(formValues.orderNumber): '',
-            onChange: (value) => handleInputChange('orderNumber', value),
-            size: 'medium'
+            key: 'id',
+            header: 'ID',
+        },
+        {
+            key: 'productionOrder',
+            header: 'Numero de orden',
+        },
+        {
+            key: 'product',
+            header: 'producto',
+        },
+        {
+            key: 'cantidad',
+            header: 'Cantidad',
+        },
+
+
+    ]
+    const handleSelectProduct = (option: SelectOption | undefined) => {
+        setSelectProduct(option);
+    };
+    const handleSelectProductionOrder = (option: SelectOption | undefined) => {
+        setSelectProductionOrder(option);
+    }
+
+    const fields: FormField[] = [
+        {
+            name: 'selectProductionOrder',
+            placeholder: 'Orden',
+            type: 'select',
+            options: productionOrders,
+            value: selectProductionOrder,
+            onChange: (option) => handleSelectProductionOrder(option),
+        },
+        {
+            name: 'selectProduct',
+            placeholder: 'Producto',
+            type: 'select',
+            options: products,
+            value: selectProduct,
+            onChange: (option) => handleSelectProduct(option),
         },
         {
             name: 'quantity',
             type: 'number',
-            label: 'Cantidad (lb)',
-            placeholder: '200',
-            value: formValues.quality !== undefined ? String(formValues.quality): '',
-            onChange: (value) => handleInputChange('quantity', value),
-            size: 'medium'
-        },
-    ];
+            label: 'paquetes',
+            placeholder: 'paquetes',
+            size: 'large',
+            value: cantidad,
+            onChange: setCantidad
+        }
+    ]
 
-    // ...
-
-// ...
-
-    const {data, loading, error: errorFetch, get, put} = useFetch('https://coffvart-backend.onrender.com/api/')
-
-    useEffect(() => {
-        get(`productionOrders/${id}?apikey=${API_KEY}`)
-    }, [id]);
-
-    useEffect(() => {
-        if (!loading) {
-            const newValues = {
-                orderNumber: data?.productionOrder.orderNumber,
-                quantity: data?.productionOrder.quantity,
+    
+        const handleAddDetail = (e: any) => {
+            e.preventDefault();
+            if (!selectProduct) {
+                alert('Debe seleccionar un insumo antes de agregar a la tabla');
+                return;
             }
-            setFormValues(newValues)
+    
+            if (!cantidad || parseInt(cantidad) <= 0) {
+                alert('Debe ingresar una cantidad válida antes de agregar insumos');
+                return;
+            }
+
+
+        //const selectedSupply = dataInsumos?.supplies?.rows?.find((supplies: any) => supplies.id === selectedSupply?.value)
+
+        const existingDetail = detalles.find(detail => detail.idProduct === selectProduct?.value);
+
+        if (existingDetail) {
+            // Si el producto ya está en el detalle, actualiza la cantidad
+            const updatedDetalles = detalles.map(detail => {
+                if (detail.idProduct === selectProduct.value) {
+                    const updatedCantidad = parseInt(detail.cantidad) + parseInt(cantidad);
+
+                    return {
+                        ...detail,
+                        cantidad: updatedCantidad,
+                    };
+                }
+                return detail;
+            });
+
+            setDetalles(updatedDetalles);
+        } else {
+            // Si el producto no está en el detalle, agrégalo como un nuevo elemento
+            const newDetail = {
+                id: detalles.length + 1,
+                productionOrder: productionOrder,
+                product: selectProduct?.label,
+                idProduct: selectProduct?.value,
+                cantidad: cantidad,
+            };
+            setDetalles([...detalles, newDetail]);
+
+            setProductionOrder('');
+            setSelectProduct(undefined);
+            setCantidad('');
+
         }
-    }, [data]);
 
-
-    const handleInputChange = (name: string, value: string | number) => {
-        setFormValues((prevValues: any) => ({
-            ...prevValues,
-            [name]: value,
-        }));
     };
+    
+    useEffect(() => {
+        getProducts(`products?apikey=${API_KEY}`);
+    }, []);
+    useEffect(() => {
+        getPO(`ProductionOrders?apikey=${API_KEY}`);
+    }, []);
 
-    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        console.log(formValues, 'esto lo voy a mandar')
+    useEffect(() => {
+        if (!loadingProducts && !errorProducts) {
+            console.log(dataProducts?.products?.rows, 'Aqui estan los Productos')
+            const optionToSelectProducts: SelectOption[] = dataProducts?.products?.rows?.map((products: any) => ({
+                value: products.id,
+                label: products.name,
+            }))
+            console.log(optionToSelectProducts, 'Aqui estan los Productos')
+            setProducts(optionToSelectProducts)
+        }
+    }, [dataProducts]);
+    
+    useEffect(() => {
+        if (!loadingPO && !errorPO) {
+            console.log(dataPO?.productionOrders?.rows, 'Aqui estan las ordenes de produccion')
+            const optionToSelectProductionOrders: SelectOption[] = dataPO?.productionOrders?.rows?.map((productionOrders: any) => ({
+                value: productionOrders.id,
+                label: productionOrders.id,
+            }))
+            console.log(optionToSelectProductionOrders, 'Aqui estan las ordenes de produccion')
+            setProductionOrders(optionToSelectProductionOrders)
+        }
+    }, [dataPO]);
+
+
+
+   
+    console.log(dataPO, 'Aqui estan las ordenes de produccion x')
+    const handleDeleteProduct = (id: string) => {
+        const updatedDetalles = detalles.filter((detalle) => detalle.id !== id);
+        setDetalles(updatedDetalles);
+    };
+    
+
+    const handleCreateIncrement = async () => {
+        console.log('Entre')
+
+        if (detalles.length === 0) {
+            alert('Debe agregar al menos un producto antes de crear la orden de producción');
+            return;
+        }
+        let id = detalles[0]
+
         const requestBody = {
-            orderNumber: formValues.orderNumber,
-            quantity: formValues.quantity,
+            productionOrder: productionOrder,
+            state: true,
+            Productsdetails: detalles.map((detalle) => ({
+                productionOrderDetailId: detalle.id,
+                productId: detalle.idProduct,
+                quantity: parseInt(detalle.cantidad),
+            })),
         };
-        console.log(requestBody, 'esto es lo que voy a mandar')
-        put(`productionOrders/${id}?apikey=${API_KEY}`, requestBody)
-        if (!errorFetch) {
-            setTimeout(() => {
-                navigate(-1)
-            }, 500);
+        console.log("esto estoy mandando", requestBody)
+
+        try {
+            const response = await fetch(`https://coffvart-backend.onrender.com/api/ProductionOrderDetail?apikey=${API_KEY}`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(requestBody),
+            });
+
+            if (!response.ok) {
+                alert('Error al crear el empaquetado');
+                console.error('Error al crear el empaquetado:', response.statusText);
+                return;
+            }
+
+            setProductionOrder('');
+            setDetalles([]);
+            setProducts([]);
+            setSelectProduct(undefined);
+            setCantidad('');
+            alert('Empaquetado creado con éxito');
+
+        } catch (error) {
+            
+            console.error('Error al crear el empaquetado:', error);
+            
+            alert('Error al crear el empaquetado');
         }
     };
 
-    if (loading) {
-        return <div>Cargando...</div>;
-    }
 
     return (
-        <Form
-            title='Editar orden de producción'
-            fields={productionOrderFields}
-            onSubmit={handleSubmit}
-            button={<Button text='Editar orden de producción' onClick={() => null} fill={true} type={'SUBMIT'}/>}
-            errors={error}
-        />
+        <Container align={'CENTER'}>
+            <Titles title={'CREAR EMPAQUETADO'}/>
+            <Container justify={'CENTER'} align={'TOP'} direction={'ROW'} gap={2}>
+                <div style={{width: '50%'}}>
+                    <Titles title={`N°${productionOrder}`} level={2} transform={'UPPERCASE'}/>
+                    <Form
+                        fields={fields}
+                        onSubmit={handleAddDetail}
+                        button={<Button text={'Agregar'} onClick={() => null} type={'SUBMIT'} fill={false}/>}
+                        cancelButton={false}
+                    />
+                </div>
+                <div style={{width: '50%', display: 'flex', flexDirection: 'column', alignItems: 'flex-end'}}>
+                    <Titles title={'Detalle del Empaquetado'} level={2} transform={'UPPERCASE'}/>
+                    <Table columns={headers} data={detalles} onRowClick={() => null}
+                           deleteAction={{onClick: handleDeleteProduct}}/>
+                    <style>
+                        {`
+                  .info {
+                    width: 100%;
+                    display: flex;
+                    flex-direction: column;
+                    align-items: flex-end;
+                    margin-top: 2rem;
+                  }
+                  .info p {
+                    font-size: 1.2rem;
+                    font-weight: bold;
+                  }
+                  .totals-table {
+                    width: 100%;
+                    border-collapse: collapse;
+                    margin-top: 1rem;
+                  }
+                  .totals-table th,
+                  .totals-table td {
+                    border: 1px solid #ddd;
+                    padding: 8px;
+                    text-align: right;
+                  }
+                  .totals-table th {
+                    background-color: #f2f2f2;
+                  }
+                `}
+                    </style>
+                    <div className="info">
+                        <table className="totals-table">
+                            <thead>
+                            <tr>
+                                <th>Concepto</th>
+                                <th>Monto</th>
+                            </tr>
+                            </thead>
+                        </table>
+                        <Button text={'Crear Compra'} onClick={() => handleCreateIncrement()} fill={false} type={'SUBMIT'}/>
+                    </div>
+                </div>
+            </Container>
+        </Container>
     );
-};
+}
 
-export default ProductionOrdersEdit;
+
+export default IncrementProducts;
