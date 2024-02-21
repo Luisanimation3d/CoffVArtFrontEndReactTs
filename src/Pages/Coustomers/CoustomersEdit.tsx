d43120ad0565c612d20c8fb4939181c9ae1b7c6c
MateoOsorio016 <mosorio7347@misena.edu.co>
2024-02-20 20:59:00 -0500

    Anañdi mas validaciones al crear y editar clientes

--- src/Pages/Coustomers/CoustomersEdit.tsx
+++ src/Pages/Coustomers/CoustomersEdit.tsx
@@ -12,7 +12,6 @@ export const CustomersEdit = () => {
     const {id} = useParams<{ id: string }>()
     const navigate = useNavigate()
     const [error, setError] = useState<{}>({})
-    const [tipo, setTipo] = useState<SelectOption | undefined>();
 
     const [optionsRoles, setOptionsRoles] = useState<SelectOption[]>([])
 
@@ -160,29 +159,37 @@ export const CustomersEdit = () => {
     const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         let mensajeError = {}
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
         if (!formData.roleId) {
             mensajeError = {...mensajeError, rol: 'El rol es requerido'}
         }
         if (!formData.documentType) {
             mensajeError = {...mensajeError, documentType: 'El tipo de documento es requerido'}
         }
-        if (!formData.documentNumber) {
-            mensajeError = {...mensajeError, documentNumber: 'El número de documento es requerido'}
+        if (!formData.documentNumber || formData.documentNumber.length < 8 || formData.documentNumber.length > 15) {
+            mensajeError = { ...mensajeError, documentNumber: 'El número de documento debe tener entre 8 y 15 caracteres' };
         }
-        if (!formData.name) {
-            mensajeError = {...mensajeError, name: 'El nombre es requerido'}
+        
+        if (!formData.name || formData.name.length < 3 || formData.name.length > 15) {
+            mensajeError = { ...mensajeError, name: 'El nombre debe tener entre 3 y 15 letras' };
         }
-        if (!formData.lastname) {
-            mensajeError = {...mensajeError, lastname: 'El apellido es requerido'}
+        if (!formData.lastname || formData.lastname.length < 3 || formData.lastname.length > 15) {
+            mensajeError = { ...mensajeError, lastname: 'El apellido debe tener entre 3 y 15 letras' };
         }
         if (!formData.address) {
             mensajeError = {...mensajeError, address: 'La dirección es requerida'}
         }
-        if (!formData.phone) {
-            mensajeError = {...mensajeError, phone: 'El teléfono es requerido'}
+        if (!formData.phone || formData.phone.length < 10 || formData.phone.length > 12) {
+            mensajeError = { ...mensajeError, phone: 'El teléfono debe tener entre 10 y 12 caracteres' };
         }
-        if (!formData.email) {
-            mensajeError = {...mensajeError, email: 'El correo electrónico es requerido'}
+        if (!formData.email || !emailRegex.test(formData.email)) {
+            mensajeError = { ...mensajeError, email: 'Ingrese un correo electrónico válido' };
+        }
+    
+
+        if (Object.keys(mensajeError).length > 0) {
+            setError(mensajeError);
+            return;
         }
         console.log(formData, 'esto lo voy a mandar')
         const requestBody = {
