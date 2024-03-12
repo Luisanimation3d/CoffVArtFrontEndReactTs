46cab55e28de36961ed5d169aa788e9016b8f269
MateoOsorio016 <mosorio7347@misena.edu.co>
2024-03-11 19:33:34 -0500

    Dashboard

--- src/components/InfoCardRedisign/InfoCardRedisign.tsx
+++ src/components/InfoCardRedisign/InfoCardRedisign.tsx
@@ -1,6 +1,8 @@
 import styles from './InfoCardRedisign.module.css'
 import {FiArrowUpCircle, FiMoreHorizontal} from "react-icons/fi";
 import {useEffect, useState} from "react";
+import { useFetch } from '../../hooks/useFetch';
+import { API_URL, API_KEY } from '../../utils/constantes';
 
 interface InfoCardProps {
     darkMode: boolean;
@@ -10,11 +12,63 @@ interface InfoCardProps {
 
 export const InfoCardRedisign = ({darkMode, direction = 'column', ...props}: InfoCardProps) => {
 
+    const [userDataSale, setUserDataSale] = useState<any>({});
+    const { data, get } = useFetch(API_URL);
+
+    useEffect(() => {
+        get(`sales?apikey=${API_KEY}`);
+    }, []);
+
+    useEffect(() => {
+        if(data?.sales){
+            setUserDataSale(data?.sales);
+        }
+    }, [data]);
+    console.log('InfoCardRedisign - userDataSale:', userDataSale);
+
+let totalVentasMes = 0;
+  let productosVendidosTotales: any = {};
+  let productoMasVendido: string | undefined;
+  let cantidadMasVendida: number | undefined;
+  let productoMenosVendido: string | undefined;
+  let cantidadMenosVendida: number | undefined;
+
+  if (userDataSale.rows) {
+    userDataSale.rows.forEach((sale: any) => {
+      totalVentasMes += sale.total;
+
+      sale.salesdetails.forEach((detail: any) => {
+        const productName = detail.product.name;
+
+        
+        if (productosVendidosTotales[productName]) {
+          productosVendidosTotales[productName] += detail.quantity;
+        } else {
+          productosVendidosTotales[productName] = detail.quantity;
+        }
+      });
+    });
+
+    const productosOrdenados = Object.keys(productosVendidosTotales).sort(
+      (a, b) => productosVendidosTotales[b] - productosVendidosTotales[a]
+    );
+
+
+    productoMasVendido = productosOrdenados[0];
+    cantidadMasVendida = productosVendidosTotales[productoMasVendido];
+
+
+    productoMenosVendido = productosOrdenados[productosOrdenados.length - 1];
+    cantidadMenosVendida = productosVendidosTotales[productoMenosVendido];
+  }
+
+    
+
     return (
         <div
             className={`${styles.infoCard__container} ${darkMode ? styles.infoCard__container__darkMode : styles.infoCard__container__lightMode}`} {...props}>
             <div className={`${styles.infoCard__header}`}>
-                <h2 className={`${styles.infoCard__header__title}`}>Producto más vendido</h2>
+                <h2 className={`${styles.infoCard__header__title}`}>Total de Ventas del Mes</h2>
                 <span className={`${styles.infoCard__header__icon}`}>
                     <FiMoreHorizontal/>
                 </span>
@@ -23,12 +77,18 @@ export const InfoCardRedisign = ({darkMode, direction = 'column', ...props}: Inf
                 <div className={`${styles.infoCard__body__data}`}>
                     <div className={`${styles.infoCard__body__data__container}`}>
                         <span className={`${styles.infoCard__body__data__price}`}>
-                            ${Intl.NumberFormat('es-CO').format(15025485)}
+                            ${Intl.NumberFormat('es-CO').format(totalVentasMes)}
                         </span>
                         <div className={`${styles.infoCard__body__data__content}`}>
-                            <h3 className={`${styles.infoCard__body__data__title}`}>Café Burdeo</h3>
+                            <h3 className={`${styles.infoCard__body__data__title}`}>{productoMasVendido}</h3>
                             <p className={`${styles.infoCard__body__data__description}`}>
-                                Este café es el más vendido de la semana, con un total de 1500 unidades vendidas.
+                                Este café es el más vendido de la semana, con un total de {cantidadMasVendida} unidades vendidas.
+                            </p>
+                        </div>
+                        <div className={`${styles.infoCard__body__data__content}`}>
+                            <h3 className={`${styles.infoCard__body__data__title}`}>{productoMenosVendido}</h3>
+                            <p className={`${styles.infoCard__body__data__description}`}>
+                                Este café es el menos vendido de la semana, con un total de {cantidadMenosVendida} unidades vendidas.
                             </p>
                         </div>
                     </div>
