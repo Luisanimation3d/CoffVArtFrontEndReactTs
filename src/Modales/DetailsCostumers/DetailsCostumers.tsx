d6e1604dc9737997228928b8decb1a0e236afc31
AlejandroVelasHel <velalejandro23@gmail.com>
2024-03-16 23:21:41 -0500

    mejora de detalle de cliente

--- src/Modales/DetailsCostumers/DetailsCostumers.tsx
+++ src/Modales/DetailsCostumers/DetailsCostumers.tsx
@@ -37,76 +37,76 @@ export const UserProfileComponent= ({setIsModalOpen, title = 'Informacion del us
         <ModalContainer ShowModal={setIsModalOpen}>
             <Modal showModal={setIsModalOpen} title={title}>
             <section className='profile-section'>
-      <MDBContainer className="py-5">
+      <MDBContainer className="py-5" style={{ backgroundColor:"rgba(299,299,299,0.2)"}}>
         <MDBRow>
-          <MDBCol lg="4">
-            <MDBCard className="mb-4 image-card">
-              <MDBCardBody className="text-center">
+          <MDBCol lg="3">
+            <MDBCard className="mb-4 image-card" style={{backgroundColor:"rgb(2,2,2,0.0)"}}>
+              <MDBCardBody className="text-center" style={{backgroundColor:'rgba(266,266,266,0.0)', borderRadius: 200}}>
                 <MDBCardImage
-                  src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava3.webp"
+                  src="https://th.bing.com/th/id/OIG4.o2w6LTpri23MMRavBLon?w=1024&h=1024&rs=1&pid=ImgDetMain"
                   alt="avatar"
                   className="rounded-circle"
-                  style={{ width: '150px' }}
+                  style={{ width: '200px'}}
                   fluid
                 />
-                <p>{userData.name}</p>
+                <p style={{color:"white", fontWeight:"bold"}}>{userData.name}</p>
               </MDBCardBody>
             </MDBCard>
           </MDBCol>
-          <MDBCol lg="8">
-            <MDBCard className="mb-4 info-card">
-              <MDBCardBody>
-                <MDBRow>
-                  <MDBCol sm="3">
-                    <MDBCardText>Nombre:</MDBCardText>
+          <MDBCol >
+            <MDBCard style={{width:"112%", marginTop:"7%"}}>
+              <MDBCardBody >
+                <MDBRow  >
+                  <MDBCol  >
+                    <MDBCardText style={{fontStyle: "oblique", marginLeft:-12, fontWeight: "bold", marginTop:"2%" }}>Nombre:</MDBCardText>
                   </MDBCol>
-                  <MDBCol sm="9">
-                    <MDBCardText>{userData?.name}</MDBCardText>
-                  </MDBCol>
-                </MDBRow>
-                <hr />
-                <MDBRow>
-                  <MDBCol sm="3">
-                    <MDBCardText>Apellido:</MDBCardText>
-                  </MDBCol>
-                  <MDBCol sm="9">
-                    <MDBCardText>{userData?.user?.lastname}</MDBCardText>
+                  <MDBCol >
+                    <MDBCardText style={{fontStyle: "italic", fontFamily:"cursive", marginLeft:"0%"}}>{userData?.name}</MDBCardText>
                   </MDBCol>
                 </MDBRow>
                 <hr />
                 <MDBRow>
-                  <MDBCol sm="3">
-                    <MDBCardText>Documento:</MDBCardText>
+                  <MDBCol >
+                    <MDBCardText style={{fontStyle: "oblique", marginLeft:-12, fontWeight: "bold", marginTop:"2%" }}>Apellido:</MDBCardText>
                   </MDBCol>
-                  <MDBCol sm="9">
-                    <MDBCardText>{userData?.document} {userData?.documentType}</MDBCardText>
-                  </MDBCol>
-                </MDBRow>
-                <hr />
-                <MDBRow>
-                  <MDBCol sm="3">
-                    <MDBCardText>Correo:</MDBCardText>
-                  </MDBCol>
-                  <MDBCol sm="9">
-                  <MDBCardText>{userData?.user?.email}</MDBCardText>
+                  <MDBCol >
+                    <MDBCardText style={{fontStyle: "italic", fontFamily:"cursive", marginLeft:"0%"}}>{userData?.user?.lastname}</MDBCardText>
                   </MDBCol>
                 </MDBRow>
                 <hr />
                 <MDBRow>
-                  <MDBCol sm="3">
-                    <MDBCardText>Telefono:</MDBCardText>
+                  <MDBCol >
+                    <MDBCardText style={{fontStyle: "oblique", marginLeft:-12, fontWeight: "bold", marginTop:"2%"}}>Documento:</MDBCardText>
                   </MDBCol>
-                  <MDBCol sm="9">
-                    <MDBCardText>{userData?.phone}</MDBCardText>
+                  <MDBCol >
+                    <MDBCardText style={{fontStyle: "italic", fontFamily:"cursive", marginRight:"0%"}}>{userData?.document} {userData?.documentType}</MDBCardText>
                   </MDBCol>
                 </MDBRow>
                 <hr />
                 <MDBRow>
-                  <MDBCol sm="3">
-                    <MDBCardText>Address:</MDBCardText>
+                  <MDBCol >
+                    <MDBCardText style={{fontStyle: "oblique", marginLeft:-12, fontWeight: "bold" , marginTop:"2%"}} >Correo:</MDBCardText>
                   </MDBCol>
-                  <MDBCol sm="9">
-                    <MDBCardText>{userData?.address}</MDBCardText>
+                  <MDBCol >
+                  <MDBCardText style={{fontStyle: "italic", fontFamily:"cursive", marginRight:"0%"}}>{userData?.user?.email}</MDBCardText>
+                  </MDBCol>
+                </MDBRow>
+                <hr />
+                <MDBRow>
+                  <MDBCol >
+                    <MDBCardText style={{fontStyle: "oblique", marginLeft:-12, fontWeight: "bold", marginTop:"2%" }}>Telefono:</MDBCardText>
+                  </MDBCol>
+                  <MDBCol >
+                    <MDBCardText style={{fontStyle: "italic", fontFamily:"cursive", marginRight:"0%"}}>{userData?.phone}</MDBCardText>
+                  </MDBCol>
+                </MDBRow>
+                <hr />
+                <MDBRow>
+                  <MDBCol >
+                    <MDBCardText style={{fontStyle: "oblique", marginLeft:-12, fontWeight: "bold", marginTop:"2%" }}>Dirección:</MDBCardText>
+                  </MDBCol>
+                  <MDBCol >
+                    <MDBCardText style={{fontStyle: "italic", fontFamily:"cursive", marginRight:"0%"}}>{userData?.address}</MDBCardText>
                   </MDBCol>
                 </MDBRow>
               </MDBCardBody>
